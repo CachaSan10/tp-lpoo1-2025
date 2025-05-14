dca321e1c9b60e2ae853beedd22556c900a56c5a
Marisol Castillo <36350945@fi.unju.edu.ar>
2025-05-14 18:58:24 -0300

    Se agregaron las Clases Periodo y Destino en el Diagrama de Clases

---
+++ ClasesBase/Periodo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClasesBase
+{
+    public class Periodo
+    {
+        private int per_codigo;
+        private string per_descripcion;
+
+        public string Per_descripcion
+        {
+            get { return per_descripcion; }
+            set { per_descripcion = value; }
+        }
+
+        public int Per_codigo
+        {
+            get { return per_codigo; }
+            set { per_codigo = value; }
+        }
+    }
+}
